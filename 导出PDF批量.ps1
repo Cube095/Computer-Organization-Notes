58dbb3bed315d103472c509e3a2db7d090c17ca9
Cube095 <2149274401@qq.com>
2026-04-11 21:37:54 +0800

    vault backup: 2026-04-11 21:37:54

--- 导出PDF批量.ps1
+++ 导出PDF批量.ps1
@@ -3,7 +3,7 @@
 
 param(
     [string]$VaultPath = "e:\计算机组成原理\Computer-Organization-Notes",
-    [string]$OutputPath = "e:\计算机组成原理\Computer-Organization-Notes\PDF_Output"
+    [string]$OutputPath = "e:\Computer-Organization-Notes\PDF_Output"
 )
 
 if (!(Test-Path $OutputPath)) {
